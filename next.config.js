31b2bdd949ea3d516d84c59c9032dd3d2a542c53
Jishnu Pavithran <jishnupavithran007@gmail.com>
2022-05-07 11:25:09 +0530

    next config chnage 4

--- next.config.js
+++ next.config.js
@@ -1,17 +1,28 @@
 /** @type {import('next').NextConfig} */
+// module.exports = {
+//   webpack5: false,
+// };
 module.exports = {
   reactStrictMode: true,
+  // other configs...
+
+  // future: { webpack5: true }, // -- not needed since Next.js v11.0.0
   webpack(config) {
     config.module.rules.push({
-      test: /\.svg$/,
-      issuer: {
-        test: /\.(js|ts)x?$/,
-        and: [/\.(js|ts)x?$/],
-      },
-
-      use: ["@svgr/webpack"],
+      test: /\.svg$/i,
+      issuer: { and: [/\.(js|ts|md)x?$/] },
+      use: [
+        {
+          loader: "@svgr/webpack",
+          options: {
+            prettier: false,
+            svgo: true,
+            svgoConfig: { plugins: [{ removeViewBox: false }] },
+            titleProp: true,
+          },
+        },
+      ],
     });
-
     return config;
   },
 };
