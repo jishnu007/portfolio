d4a9d303a8101cdbf47f069c1e4d089d73f87425
Jishnu Pavithran <jishnupavithran007@gmail.com>
2022-05-07 12:20:41 +0530

    config revert

--- next.config.js
+++ next.config.js
@@ -1,28 +1,4 @@
 /** @type {import('next').NextConfig} */
-// module.exports = {
-//   webpack5: false,
-// };
 module.exports = {
   reactStrictMode: true,
-  // other configs...
-
-  // future: { webpack5: true }, // -- not needed since Next.js v11.0.0
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: { and: [/\.(js|ts|md)x?$/] },
-      use: [
-        {
-          loader: "@svgr/webpack",
-          options: {
-            prettier: false,
-            svgo: true,
-            svgoConfig: { plugins: [{ removeViewBox: false }] },
-            titleProp: true,
-          },
-        },
-      ],
-    });
-    return config;
-  },
 };
