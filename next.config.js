08c98cf9a30a431d9075a9d9bf1ea7c6cd50cdfc
Jishnu Pavithran <jishnupavithran007@gmail.com>
2022-05-07 11:10:18 +0530

    update next config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,25 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  webpack5: false,
+  webpack(config, options) {
+    const fileLoaderRule = config.module.rules.find(
+      (rule) => rule.test && rule.test.test(".svg")
+    );
+    fileLoaderRule.exclude = /\.svg$/;
+
+    config.module.rules.push({
+      loader: "@svgr/webpack",
+      options: {
+        prettier: false,
+        svgo: true,
+        svgoConfig: {
+          plugins: [{ removeViewBox: false }],
+        },
+        titleProp: true,
+      },
+      test: /\.svg$/,
+    });
+
+    return config;
+  },
 };
