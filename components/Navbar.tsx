168a9ecaf97dd24f02228948b62888e3464b2ba2
Jishnu Pavithran <jishnupavithran007@gmail.com>
2022-05-08 20:01:17 +0530

    all config updated

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 import Image from "next/image";
 import Link from "next/link";
-import Logo from "../public/logo.svg";
+import Logo from "../assets/images/logo.svg";
 const Navbar = () => {
   return (
     <div className="absolute z-10 px-16 navbar justify-between flex w-full">
@@ -11,9 +11,7 @@ const Navbar = () => {
         data-aos-easing="linear"
         data-aos-duration="700"
       >
-        <img src="/logo.svg" alt="logo" width="180px" height="90px" />
-        {/* <Image src={Logo} alt="logo" width={180} height={90} /> */}
-        {/* <Logo width={180} height={90} /> */}
+        <Image src={Logo} alt="logo" width={180} height={90} />
       </div>
       <ul className="navbar__nav flex text-center items-center font-bold text-xl">
         <li className="px-8">
