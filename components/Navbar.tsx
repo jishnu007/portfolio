02beb34c4b7769c0afa6bcecbb7aed017a6776a4
Jishnu Pavithran <jishnupavithran007@gmail.com>
2022-05-18 16:01:39 +0530

    some minor updates

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 import Image from "next/image";
-import Link from "next/link";
+
 import Logo from "../assets/images/logo.svg";
 import Hamburger from "../assets/images/hamburger.svg";
 import { Drawer, Button, Carousel } from "antd";
